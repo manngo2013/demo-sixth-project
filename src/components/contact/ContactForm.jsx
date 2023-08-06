0d20796f035f14513d9f40b2d3de1f78f666c9fd
manngo <manngo2013@gmail.com>
2023-08-06 21:52:07 +0700

    Lecture 70: Using React Hook Form

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,35 +1,60 @@
 import React from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import styles from "./contactForm.module.scss";
+import { useForm } from "react-hook-form";
 
 function ContactForm(props) {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div>
       <h2 className={styles.title}>Contact Us</h2>
-      <Form className={styles.form}>
+      <Form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
         <Row>
           <Form.Group as={Col} sm={{ span: 6 }} controlId="contact-name">
-            <Form.Control type="text" placeholder="Your name" />
+            <Form.Control
+              {...register("name")}
+              type="text"
+              placeholder="Your name"
+            />
           </Form.Group>
           <Form.Group as={Col} sm={{ span: 6 }} controlId="contact-phone">
-            <Form.Control type="number" placeholder="Your phone" />
+            <Form.Control
+              {...register("phone")}
+              type="number"
+              placeholder="Your phone"
+            />
           </Form.Group>
         </Row>
         <Row>
           <Form.Group as={Col} sm={{ span: 6 }} controlId="contact-email">
-            <Form.Control type="email" placeholder="Your email" />
+            <Form.Control
+              {...register("email")}
+              type="email"
+              placeholder="Your email"
+            />
           </Form.Group>
           <Form.Group as={Col} sm={{ span: 6 }} controlId="contact-subject">
-            <Form.Control type="text" placeholder="Your subject" />
+            <Form.Control
+              {...register("subject")}
+              type="text"
+              placeholder="Your subject"
+            />
           </Form.Group>
         </Row>
         <Row>
           <Form.Group controlId="contact-message">
-            <Form.Control as="textarea" rows={3} placeholder="Your message" />
+            <Form.Control
+              {...register("message")}
+              as="textarea"
+              rows={3}
+              placeholder="Your message"
+            />
           </Form.Group>
         </Row>
         <div className="text-center">
-          <Button type="button" size="md" variant="outline-secondary">
+          <Button type="submit" size="md" variant="outline-secondary">
             Send
           </Button>
         </div>
